d9ec6f2288c7b3002a05f5ab09b7e609c01c5b28
emilaimi <emilaimi12@gmail.com>
2024-04-07 15:20:59 +0200

    expensetracker

---
+++ expenses/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.CharField(max_length=255)
+    date = models.DateField(auto_now_add=True)
+    def __str__(self):
+        return self.description
+    
+
---
+++ expenses/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from .models import Expense
+from .forms import ExpenseForm
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def expense_list(request):
+    expenses = Expense.objects.filter(user=request.user)
+    return render(request, 'expenses/expense_list.html', {'expenses': expenses})
+
+@login_required
+def add_expense(request):
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            expense = form.save()
+            expense.user = request.user
+            expense.save()
+            return redirect('expense_list')
+    else:
+        form = ExpenseForm()
+    return render(request, 'expenses/add_expense.html', {'form': form})
